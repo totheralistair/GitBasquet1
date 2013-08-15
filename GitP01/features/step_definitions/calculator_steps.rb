dcff511a84bc23b251b221b1bf5a8f24b4d5b5d3
totheralistair <totheralistair@aol.com>
2013-08-15 13:21:05 +0200

    1st cucumber test in rubymine

--- GitP01/features/step_definitions/calculator_steps.rb
+++ GitP01/features/step_definitions/calculator_steps.rb
@@ -1,9 +1,11 @@
 Given /^the input "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @input = arg1
 end
 When /^the calculator is run$/ do
-  pending # express the regexp above with the code you wish you had
+  @output = `ruby calc.rb #{@input}`
+  raise('Command failed!') unless $?.success?
 end
-Then /^the output should be "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^the output should be "([^"]*)"$/ do |arg2|
+  @output.should == arg2
+
 end
