f20b7eac66b17fa3dfa9988ea3db0c7f50a1ce6a
totheralistair <totheralistair@aol.com>
2013-10-02 17:11:20 +0300

    starting webless tests
    wanting my system to work w no web drivers (but still using web-style
    'request/response' protocol

---
+++ GitP07/src/basquet.rb
@@ -0,0 +1,37 @@
+
+class Basquet
+# ======= CLASS METHODS =======
+  def self.aPersistentBasquet
+    @@theOneAndOnlyBasquet ||= Basquet.new
+  end
+
+  def self.newPersistentBasquetPlease
+    @@theOneAndOnlyBasquet = Basquet.new
+  end
+
+# ======= INSTANCE METHODS =======
+  def initialize
+    @contents = Array.new
+  end
+
+  def zadd(something)    #returns index of thing added
+    @contents.push(something).size - 1
+  end
+
+  def gimmeAt(index)
+    @contents[index]
+  end
+
+  def gimmeAll
+    @contents
+  end
+
+  def gimmeLast(how_many=1)
+    @contents.last(how_many)
+  end
+
+  def size
+    @contents.size
+  end
+
+end
