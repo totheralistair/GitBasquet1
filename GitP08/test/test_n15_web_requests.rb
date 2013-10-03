271366755ea01aff32b5353b2c4a1ccee3c11c9b
totheralistair <totheralistair@aol.com>
2013-10-03 12:10:34 +0300

    Starting P08, new MuffinLand interface
    redesigning the i/f to mL so it looks more like a real CMS should,
    taking out all the intermediate return strings. this commit is the zero
    point, before I make any changes to the i/f

--- GitP08/test/test_n15_web_requests.rb
+++ GitP08/test/test_n15_web_requests.rb
@@ -1,42 +1,46 @@
 require 'rspec/expectations'
 require 'test/unit'
-require_relative '../src/muffinland_verbs'
+require 'rack/test'
+require_relative '../src/n15_req_thru_sinatra'
 
+class TestRequestsToBasquet < Test::Unit::TestCase
+    include Rack::Test::Methods
 
-
-class TestWeblessRequestsToBasquet < Test::Unit::TestCase
-  def setup
+  def app
+    Sinatra::Application
   end
 
-  def teardown
+  def test_00_hello
+    get '/'
+      last_response.body.should == "Welcome to Muffinland & my little toy Basquet"
   end
-
-
-#  def test_00_muffinland_home #THERE is no _home method on ML
-#  end
 
   def test_01_fresh_basquet_is_empty
-    muffinland_fresh_DB.should == "New basquet w 0 items."
+    get "/FRESH_DB"
+      last_response.body.should == "New basquet w 0 items."
   end
 
-  def test_02_storing_full_request
-    muffinland_fresh_DB.should == "New basquet w 0 items."
-    muffinland_addGETRequest("/addGETRequest/broohaha").should == "addGETRequest request stored at 0"
-  end
+    def test_02_storing_full_request
+      get "/FRESH_DB"; last_response.body.should == "New basquet w 0 items."
+      get "/addGETRequest/broohaha"
+      last_response.body.should == "addGETRequest request stored at 0"
+    end
 
-  def test_02b_correct_persistence_of_basquet_across_tests
-    muffinland_fresh_DB.should == "New basquet w 0 items."
-    muffinland_addGETRequest("/addGETRequest/broohaha").should == "addGETRequest request stored at 0"
-    req = Net::HTTP::Get.new('http://bogus.uri')
-    req['PATH_INFO']= 'something'
-    req.body = 'hi'
-    muffinland_addGETRequest(req)
-    muffinland_getRequestVerb_at(1).should == "something"
-  end
+    def test_02b_correct_persistence_of_basquet_across_tests
+      puts "starting"
+      get "/FRESH_DB"; last_response.body.should == "New basquet w 0 items."
+      get "/addGETRequest/broohaha"
+      last_response.body.should == "addGETRequest request stored at 0"
+    #  #puts "-3-"
+      get '/getRequestVerbAt/0'
+    #  #puts "3.", last_response.inspect
+      last_response.body.should == "/addGETRequest/broohahax"
+    end
 
 =begin
+#        last_response.body.should == "/addGETRequest/broohaha"
     get "/addGETRequest/GrimmStories"
-        last_response.body.should == "addGETRequest request stored at 1"
+#        last_response.body.should == "addGETRequest request stored at 1"
     get '/getRequestVerbAt/1'
         last_response.body.should == "/addGETRequest/GrimmStories"
   def test_03_storing_POST_request
@@ -47,6 +51,8 @@ class TestWeblessRequestsToBasquet < Test::Unit::TestCase
        last_response.body.should == "oogaPOSTboogo"
   end
 
+=begin
+=begin
   def test_cookie_gets_added_if_it_does_not_exist
     #  SEE MOM, NO COOKIE!
     get "/FRESH_DB"; last_response.body.should == "New basquet w 0 items."
