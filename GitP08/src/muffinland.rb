be68b6d3c12224557ce2587fbdc0995450eaf365
totheralistair <totheralistair@aol.com>
2013-10-09 16:36:02 +0200

    added cookies to simplified muffinland
    don't understand cookies to save my life, but they're back in as they
    were before. Time to learn something different.

--- GitP08/src/muffinland.rb
+++ GitP08/src/muffinland.rb
@@ -1,5 +1,4 @@
-=begin
-MuffinLand is a content management application serving web requests.
+=begin MuffinLand is a content management application serving web requests.
 From whatever source, web or not, it expects incoming data to be HTTP requests (GET or POST)
 for now, it is tied to Rack type requests.
 =end
@@ -11,19 +10,29 @@ def ml_fresh_DB  # SCARY! USE ONLY FOR TESTING, NOT PRODUCTION!!
   out = $requests.size.to_s
 end
 
-def ml_add_request(request)
+def ml_add(request)
+# i dont' understand cookies at all.
+  if request.cookies.empty?
+    response.set_cookie( "user_session", :value => "user 123")
+  end
   $requests.push(request)
   out = ($requests.size - 1).to_s
 end
 
-def ml_request_at(location)
+def ml_at(location)
   req = $requests[ location ]
   out = req
 end
 
-def ml_request_path_at(location)
-  req = ml_request_at( location ) # in theory, is a Rack request, but who really knows?
+def ml_path_at(location)
+  req = ml_at( location ) # in theory, is a Rack request, but who really knows?
   path = req.env["PATH_INFO"]     # :( Rack dependency
   out = path
+end
+
+def ml_data_for_at( key, location)
+  req = ml_at( location ) # in theory, is a Rack request, but who really knows?
+  data = req.env["rack.request.form_hash"] [ key ] # :( Rack dependency
+  out = data
 end
 
--- GitP08/test/test_n16_web_requests.rb
+++ GitP08/test/test_n16_web_requests.rb
@@ -30,6 +30,26 @@ class TestRequestsToBasquet < Test::Unit::TestCase
     get '/path/0'; last_response.body.should == "/get/broohaha"
   end
 
+  def test_03_can_extract_data_field
+    get "/FRESH_DB"; last_response.body.should == "0"
+    post '/post', 'acData=oogaPOSTboogo'; last_response.body.should == "0"
+    get "/dataforat/acData/0"; last_response.body.should == "oogaPOSTboogo"
+  end
+
+  def test_cookie_gets_added_if_it_does_not_exist
+    #  SEE MOM, NO COOKIE!
+    get "/FRESH_DB"; last_response.body.should == "0"
+    post '/post'; last_response.body.should == "0"
+    last_response.original_headers["Set-Cookie"].should == "user_session=user+123"
+  end
+
+  def test_cookie_found_if_it_already_exists
+    set_cookie "user_session=user 567"
+    get "/FRESH_DB";  last_response.body.should == "0"
+    get '/get/whatever'; last_response.body.should == "0"
+    last_request.cookies.should  == { "user_session" => "user 567"}
+  end
+
 
 end
 
